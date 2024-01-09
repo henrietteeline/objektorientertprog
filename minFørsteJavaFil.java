f50a4db8532a5c085f59d5c4bc94a920936567ab
henrietteeline <156126154+henrietteeline@users.noreply.github.com>
2024-01-09 20:41:04 +0100

    Update minFørsteJavaFil.java

--- minFørsteJavaFil.java
+++ minFørsteJavaFil.java
@@ -1,6 +1,6 @@
 public class MinFørsteJavaFil {
 	public static void main(String[] args) {
-		System.out.println("Victor er digg");
+		System.out.println("Victor er digg - men det er Henriette også");
 	}
 }
 
